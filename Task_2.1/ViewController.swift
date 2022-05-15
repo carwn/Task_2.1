a8dce612994b8376312722c9725fe52a591172fc
Alexander Shelikhov <carwn@bk.ru>
2022-05-15 19:19:23 +0700

    Initial Commit

---
+++ Task_2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_2.1
+//
+//  Created by Александр Шелихов on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
